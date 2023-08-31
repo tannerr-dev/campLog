b5c366de58e9e58cf87a108887b8ec570c265f57
Tanner Reyhons <tannerr.dev@gmail.com>
2023-08-30 22:25:34 -0500

    connected

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
+
 const express = require('express');
-const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
+const Campground = require('./models/campground');
+
+
+
+mongoose.connect('mongodb://127.0.0.1:27017/campLog', {
+    useNewUrlParser: true,
+    // useCreateIndex: true,// docs says it always acts as true now
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"))
+db.once("open", ()=>{
+    console.log("DataBASS connected")
+});
+
+const app  = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
@@ -11,6 +29,14 @@ app.get('/', (req, res)=>{
     // res.send('Hello, I am campLog');
     res.render('home')
 })
+
+
+app.get('/newcampground', async (req, res)=>{
+    // const camp = new Campground({title: 'Como Park', description: 'Beautiful'});
+    await camp.save();
+    res.send(camp)
+});
+
 
 
 
