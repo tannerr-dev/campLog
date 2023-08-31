8d438431045edb77180a6cf8592bd26ad0e53886
Tanner Reyhons <tannerr.dev@gmail.com>
2023-08-30 21:47:57 -0500

    campLog init

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+
+
+
+app.get('/', (req, res)=>{
+    // res.send('Hello, I am campLog');
+    res.render('home')
+})
+
+
+
+
+app.listen(3000, ()=>{
+    console.log('Listening for data waves on port 3000')
+})
