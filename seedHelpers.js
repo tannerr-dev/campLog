2f9425c98215e8d43481e8d1f09b3746cc853f44
Tanner Reyhons <tannerr.dev@gmail.com>
2023-08-30 22:40:10 -0500

    prepped for seed drop

---
+++ seedHelpers.js
@@ -0,0 +1,43 @@
+const desciptors = [
+    'Forest',
+    'Ancient',
+    'Petrified',
+    'Roaring',
+    'Cascade',
+    'Tumbling',
+    'Silent',
+    'Redwood',
+    'Bullfrog',
+    'Maple',
+    'Misty',
+    'Elk',
+    'Grizzly',
+    'Ocean',
+    'Sea',
+    'Dusty',
+    'Diamond'
+]
+
+const places = [
+    "Flats",
+    "Village",
+    "Canyon",
+    "Pond",
+    "Group Camp",
+    "Horse Camp",
+    "Ghost Town",
+    "Camp",
+    "Dispersed Camp",
+    "Backcountry",
+    "River",
+    "Creek",
+    "Creekside",
+    "Bay",
+    "Spring",
+    "Bayshore",
+    "Sands",
+    "Mule Camp",
+    "Hunting Camp",
+    "Cliffs",
+    "Hollow"
+]
