18ac82f20d66afc968c32dcc17a039b388cad71c
Tanner Reyhons <tannerr.dev@gmail.com>
2023-09-05 13:20:03 -0500

    all campgrounds

--- app.js
+++ app.js
@@ -28,12 +28,16 @@ app.get('/', (req, res)=>{
     res.render('home');
 });
 
-
-app.get('/newcampground', async (req, res)=>{
-    // const camp = new Campground({title: 'Como Park', description: 'Beautiful'});
-    await camp.save();
-    res.send(camp);
+app.get('/campgrounds', async (req, res)=>{
+    const campgrounds = await Campground.find({});
+    res.render("campgrounds/index", {campgrounds})
 });
+
+// app.get('/newcampground', async (req, res)=>{
+//     // const camp = new Campground({title: 'Como Park', description: 'Beautiful'});
+//     await camp.save();
+//     res.send(camp);
+// });
 
 
 
