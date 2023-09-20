4ee6e9735a711ad885938ea79703f22f1addb50e
Tanner Reyhons <tannerr.dev@gmail.com>
2023-09-19 21:25:41 -0500

    so much hate

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const mongoose = require('mongoose');
 const Campground = require('./models/campground');
 const methodOverride = require("method-override");
 const morgan = require("morgan");
+const ejsMate = require("ejs-mate");
+
 
 mongoose.connect('mongodb://127.0.0.1:27017/campLog', {
     useNewUrlParser: true,
@@ -20,6 +22,7 @@ db.once("open", ()=>{
 
 const app  = express();
 
+app.engine("ejs", ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
@@ -28,9 +31,16 @@ app.use(express.urlencoded({extended: true})) // parses the req.body from the se
 app.use(methodOverride("_method"));
 app.use(morgan('common'));
 
+// app.use((req, res, next)=>{
+//     req.requestTime = Date.now();
+//     console.log(req.method, req.path);
+//     next();
+// })
+
 app.get('/', (req, res)=>{
     // res.send('Hello, I am campLog');
     res.render('home');
+    // console.log(`yer request date: ${req.requestTime}`);
 });
 
 
