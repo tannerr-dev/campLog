82522dab3a76f59c685a855f4b7b72d2d9ac86e3
Tanner Reyhons <tannerr.dev@gmail.com>
2023-09-23 22:11:03 -0500

    prettier

--- app.js
+++ app.js
@@ -1,92 +1,87 @@
-
-const express = require('express');
-const path = require('path');
-const mongoose = require('mongoose');
-const Campground = require('./models/campground');
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+const Campground = require("./models/campground");
 const methodOverride = require("method-override");
 const morgan = require("morgan");
 const ejsMate = require("ejs-mate");
 
-
-mongoose.connect('mongodb://127.0.0.1:27017/campLog', {
+mongoose.connect("mongodb://127.0.0.1:27017/campLog", {
     useNewUrlParser: true,
-     useUnifiedTopology: true
+    useUnifiedTopology: true,
 }); // useCreateIndex: true,// docs says it always acts as true now
-   
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", ()=>{
+db.once("open", () => {
     console.log("DataBASS connected");
 });
 
-const app  = express();
+const app = express();
 
 app.engine("ejs", ejsMate);
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-
-app.use(express.urlencoded({extended: true})) // parses the req.body from the sent form
+app.use(express.urlencoded({ extended: true })); // parses the req.body from the sent form
 app.use(methodOverride("_method"));
-app.use(morgan('common'));
-
+app.use(morgan("common"));
 
 //middleware with next function
-app.use((req, res, next)=>{
+app.use((req, res, next) => {
     req.requestTime = Date.now();
     // console.log(req.method, req.path);
-    console.log(req.request)
+    console.log(req.request);
     next();
-})
+});
 
-app.get('/', (req, res)=>{
+app.get("/", (req, res) => {
     // res.send('Hello, I am campLog');
-    res.render('home');
+    res.render("home");
     // console.log(`yer request date: ${req.requestTime}`);
 });
 
-
-app.get('/campgrounds', async (req, res)=>{
+app.get("/campgrounds", async (req, res) => {
     const campgrounds = await Campground.find({});
-    res.render("campgrounds/index", {campgrounds})
+    res.render("campgrounds/index", { campgrounds });
 });
 
-app.get("/campgrounds/new", (req, res)=>{
-    res.render("campgrounds/new")
-}) 
+app.get("/campgrounds/new", (req, res) => {
+    res.render("campgrounds/new");
+});
 //order matters, this needs to be before the /:id otherwise the 'new' will be handled as an id
 
-app.post("/campgrounds", async (req, res)=>{
+app.post("/campgrounds", async (req, res) => {
     const campground = new Campground(req.body.campground);
-    await campground.save(); 
-    res.redirect(`/campgrounds/${campground._id}`)
-
-})
-
-app.get("/campgrounds/:id", async (req,res)=>{
-    const campground = await Campground.findById(req.params.id)
-    res.render("campgrounds/show", {campground})
- })
-
-app.get("/campgrounds/:id/edit", async (req, res)=>{
-    const campground = await Campground.findById(req.params.id)
-    res.render("campgrounds/edit", {campground})
-})
-
-app.put("/campgrounds/:id", async(req, res)=>{
-    // res.send("it worked")
-    const{ id } = req.params;
-    const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground})
+    await campground.save();
     res.redirect(`/campgrounds/${campground._id}`);
-})
+});
 
-app.delete("/campgrounds/:id", async (req, res)=>{
-    const { id }= req.params;
+app.get("/campgrounds/:id", async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render("campgrounds/show", { campground });
+});
+
+app.get("/campgrounds/:id/edit", async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render("campgrounds/edit", { campground });
+});
+
+app.put("/campgrounds/:id", async (req, res) => {
+    // res.send("it worked")
+    const { id } = req.params;
+    const campground = await Campground.findByIdAndUpdate(id, {
+        ...req.body.campground,
+    });
+    res.redirect(`/campgrounds/${campground._id}`);
+});
+
+app.delete("/campgrounds/:id", async (req, res) => {
+    const { id } = req.params;
     await Campground.findByIdAndDelete(id);
-    res.redirect("/campgrounds")
-})
+    res.redirect("/campgrounds");
+});
 
-app.listen(3000, ()=>{
-    console.log('Listening for data waves on port 3000')
-})
+app.listen(3000, () => {
+    console.log("Listening for data waves on port 3000");
+});
