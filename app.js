4aa5285c544ddc6b1aecd22ac99f4ad7666437f0
Tanner Reyhons <tannerr.dev@gmail.com>
2023-10-20 18:59:37 -0500

    add flash error

--- app.js
+++ app.js
@@ -69,6 +69,7 @@ app.use((req, res, next) => {
     next();
 });
 
+// this is FLASH setup
 app.use((req,res, next)=>{
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
