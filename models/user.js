2e9a8a0d8a70b32ccacf5217fe9b2ff825124ed9
Tanner Reyhons <tannerr.dev@gmail.com>
2023-10-22 12:27:58 -0500

    add user model

---
+++ models/user.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const passportLocalMongoose = require("passport-local-mongoose");
+
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    email:{
+        type: String,
+        required: true
+    }
+
+});
+
+UserSchema.plugin(passportLocalMongoose);
