bd7aa345f59274b3e85b5fe829d376abbef858fc
Tanner Reyhons <tannerr.dev@gmail.com>
2023-10-29 15:06:28 -0500

    add register route with error handling

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,25 @@
 const express = require("express");
 const router = express.Router();
+const catchAsync = require("../utils/catchAsync");
 const User = require("../models/user");
 
 
 router.get("/register", (req, res)=>{
     res.render("users/register");
-})
+});
 
+router.post("/register", catchAsync( async (req , res) => {
+ 
+        const {email, username, password}= req.body;
+        const user = new User({email, username});
+        const registeredUser = User.register(user, password); //User.register is a passport feature i believe
+        req.flash("success", "Welcome to campLog");
+        res.redirect("/campgrounds");
+   
+        // req.flash("error", e.message);
+        // res.redirect("/register");
+ 
+
+}));
 
 module.exports = router;
