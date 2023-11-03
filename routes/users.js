6e88a58b3d749d58cb2bd4b976fdcb59d33d9694
Tanner Reyhons <tannerr.dev@gmail.com>
2023-11-02 21:51:59 -0500

    add logout route and logic

--- routes/users.js
+++ routes/users.js
@@ -26,7 +26,16 @@ router.post("/register", catchAsync( async (req , res) => {
 router.get("/login", (req,res)=>{
     res.render("users/login");
 
-}) 
+})
+router.get('/logout', (req, res, next) => {
+    req.logout(function (err) {
+        if (err) {
+            return next(err);
+        }
+        req.flash('success', 'Goodbye!');
+        res.redirect('/campgrounds');
+    });
+}); 
 
 router.post("/login", passport.authenticate("local", {failureFlash: true, failureRedirect: "/login"}), (req, res)=>{
     req.flash("success", "Welcome back!");
