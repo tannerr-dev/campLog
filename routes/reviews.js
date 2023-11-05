0062f08065531f7d2de9c33e1676c0847d71754f
Tanner Reyhons <tannerr.dev@gmail.com>
2023-11-04 20:39:57 -0500

    semicolons

--- routes/reviews.js
+++ routes/reviews.js
@@ -17,16 +17,16 @@ router.post("/",isLoggedIn, validateReview, catchAsync( async( req, res )=>{
     campground.reviews.push(review);
     await review.save();
     await campground.save();
-    req.flash("success", "Created new review.")
+    req.flash("success", "Created new review.");
     res.redirect(`/campgrounds/${campground._id}`);
 }));
 
 router.delete("/:reviewId",isLoggedIn,isReviewAuthor, catchAsync( async(req, res)=>{
     const { id, reviewId } = req.params;
-    await Campground.findByIdAndUpdate(id, {$pull:{ reviews: reviewId}})
+    await Campground.findByIdAndUpdate(id, {$pull:{ reviews: reviewId}});
     await Review.findByIdAndDelete(reviewId);
-    req.flash("success", "Deleted review.")
-    res.redirect(`/campgrounds/${id}`)
+    req.flash("success", "Deleted review.");
+    res.redirect(`/campgrounds/${id}`);
 }));
 
 module.exports = router;
