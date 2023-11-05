aaa62e195783c3dfdadb0d98846ee06a1e6621b0
Tanner Reyhons <tannerr.dev@gmail.com>
2023-11-05 12:38:59 -0600

    refactor routes using router.route method

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -8,25 +8,19 @@ const {isLoggedIn, validateCampground, isAuthor} = require("../middleware");
 
 const campgrounds = require("../controllers/campgrounds")
 
-
-
-
-router.get("/", catchAsync(campgrounds.index));
+router.route("/")
+    .get(catchAsync(campgrounds.index))
+    .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
 
 router.get("/new", isLoggedIn, campgrounds.renderNewForm);
 //order matters, this needs to be before the /:id otherwise the 'new' will be handled as an id
 
-
 //THESE ARE THE CAMPGROUND ROUTES
-router.post("/", isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
-
-router.get("/:id", catchAsync(campgrounds.showCampground));
+router.route("/:id")
+    .get(catchAsync(campgrounds.showCampground))
+    .put(isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground))
+    .delete(isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
 
 router.get("/:id/edit", isLoggedIn, isAuthor, catchAsync(campgrounds.renderEditForm));
-
-router.put("/:id", isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.updateCampground));
-
-router.delete("/:id", isLoggedIn, isAuthor, catchAsync(campgrounds.deleteCampground));
-
 
 module.exports = router;
