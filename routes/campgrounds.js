9d6ca528039fef3fb133f98ba3b6a3891edbb7a3
Tanner Reyhons <tannerr.dev@gmail.com>
2023-11-16 20:45:02 -0600

    small route update

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,16 +1,20 @@
 const express = require("express");
 const router = express.Router();
 const catchAsync = require("../utils/catchAsync");
-
 const Campground = require("../models/campground");
-
 const {isLoggedIn, validateCampground, isAuthor} = require("../middleware");
-
 const campgrounds = require("../controllers/campgrounds")
+const multer = require("multer");
+const {storage}=require("../cloudinary"); //node automatically searches for index.js in a folder so there is no need to specify the filename
+const upload = multer({storage});
 
 router.route("/")
     .get(catchAsync(campgrounds.index))
-    .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
+    // .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
+    .post(upload.array("image"), (req, res)=>{
+        // console.log(req.body, req.files)
+        res.send("It worked.")
+    })
 
 router.get("/new", isLoggedIn, campgrounds.renderNewForm);
 //order matters, this needs to be before the /:id otherwise the 'new' will be handled as an id
